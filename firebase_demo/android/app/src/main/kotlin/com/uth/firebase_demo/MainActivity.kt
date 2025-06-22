7c8e44895f055e270d9f4db51ada2834159d433b
MaiAnhQuan <maianhquan00@gmail.com>
2025-06-22 21:20:23 +0700

    Add files via upload

---
+++ firebase_demo/android/app/src/main/kotlin/com/uth/firebase_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.uth.firebase_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ handling_api_data/android/app/src/main/kotlin/com/uth/handling_api_data/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.uth.handling_api_data
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
